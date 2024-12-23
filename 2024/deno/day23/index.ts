dece18ba0b0bf8abb9c24a38abcefe9041a7dc56
Toil <62353659+ilyhalight@users.noreply.github.com>
2024-12-23 19:48:14 +0300

    added day23 solution with deno

---
+++ 2024/deno/day23/index.ts
@@ -0,0 +1,117 @@
+import { getFile } from "../../../utils/deno/utils.ts";
+
+const input = await getFile(import.meta.dirname!);
+
+const connections = input.replaceAll("\r", "").split("\n");
+
+const connectionTree = new Map<string, string[]>();
+const alreadyConnected = new Set();
+
+for (const connection of connections) {
+  const [left, right] = connection.split("-");
+  const oldConnections2 = connectionTree.get(right);
+  connectionTree.set(
+    right,
+    oldConnections2 ? [...oldConnections2, left] : [left]
+  );
+
+  const oldConnections = connectionTree.get(left);
+  connectionTree.set(
+    left,
+    oldConnections ? [...oldConnections, right] : [right]
+  );
+  alreadyConnected.add(right);
+}
+
+const checkedClients = new Set();
+
+const checkTree = (client: string, parent = "", depth = 0) => {
+  const pairs: string[] = [];
+  if (depth > 1) {
+    return pairs;
+  }
+
+  const childrens = connectionTree.get(client)!;
+  if (!childrens.find((child) => child === parent)) {
+    return pairs;
+  }
+
+  return childrens
+    .filter((child) => child !== parent)
+    .map((child) => {
+      checkedClients.add(child);
+      const childConnections = connectionTree.get(child);
+      return childConnections!.includes(parent) ? child : false;
+    })
+    .filter((child) => child !== false);
+};
+
+const pairs = [];
+
+for (const [connection, childrens] of connectionTree.entries()) {
+  if (!connection.startsWith("t")) {
+    continue;
+  }
+
+  checkedClients.clear();
+  const pairConnections = new Set<string>();
+  for (const child of childrens) {
+    const childData = checkTree(child, connection);
+    for (const data of childData) {
+      pairConnections.add(data);
+    }
+  }
+
+  const arrPairConnectionss = Array.from(pairConnections);
+  for (let i = 0; i < arrPairConnectionss.length; i++) {
+    const pair = arrPairConnectionss[i];
+    for (let j = i + 1; j < arrPairConnectionss.length; j++) {
+      const nextPair = arrPairConnectionss[j];
+      if (connectionTree.get(nextPair)!.includes(pair)) {
+        pairs.push([connection, pair, nextPair].sort().join(","));
+      }
+    }
+  }
+}
+
+const total = new Set(pairs);
+const passwords = new Map();
+for (const [connection, childrens] of connectionTree.entries()) {
+  checkedClients.clear();
+  const pairConnections = new Set<string>();
+  for (const child of childrens) {
+    const childData = checkTree(child, connection);
+    for (const data of childData) {
+      pairConnections.add(data);
+    }
+  }
+
+  const arrPairConnectionss = Array.from(pairConnections);
+
+  const temp = [];
+  for (let i = 0; i < arrPairConnectionss.length; i++) {
+    const pair = arrPairConnectionss[i];
+    if (connectionTree.get(pair)!.includes(connection)) {
+      temp.push(pair);
+    }
+  }
+
+  const key = [connection, ...temp].sort().join(",");
+  passwords.set(key, passwords.has(key) ? passwords.get(key) + 1 : 1);
+}
+
+const totalPartTwo = passwords.entries().reduce(
+  ([password, count], [key, val]) => {
+    if (val > count) {
+      password = key;
+      count = val;
+    }
+
+    return [password, count];
+  },
+  ["", 0]
+);
+
+console.log(`Part 1: ${total.size}`);
+
+console.log(`Part 2: ${totalPartTwo[0]}`);
