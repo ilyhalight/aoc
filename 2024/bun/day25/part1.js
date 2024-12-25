6aef37f68c77ecbd3ea03904afc4609f45a7b758
Toil <62353659+ilyhalight@users.noreply.github.com>
2024-12-25 09:39:55 +0300

    added day25 part1 solution with bun

---
+++ 2024/bun/day25/part1.js
@@ -0,0 +1,59 @@
+import { getFile } from "../../../utils/bun/utils.js";
+
+const input = await getFile(import.meta.dir);
+
+const schema = input
+  .replaceAll("\r", "")
+  .split("\n\n")
+  .map((item) => item.split("\n"));
+
+const calcHeight = (item) => {
+  const columns = [];
+  const maxHeight = item.length;
+  for (let x = 0; x < item[0].length; x++) {
+    let columnHeight = -1;
+    for (let y = 0; y < maxHeight; y++) {
+      const value = item[y][x];
+      if (value === "#") {
+        columnHeight++;
+      }
+    }
+
+    columns.push(columnHeight);
+  }
+
+  return [maxHeight - 2, columns];
+};
+
+const locks = schema.filter((item) => item[0][0] === "#").map(calcHeight);
+const keys = schema.filter((item) => item[0][0] === ".").map(calcHeight);
+
+let total = 0;
+
+for (const key of keys) {
+  const [maxHeightKey, [zeroKey, firstKey, secondKey, thirdKey, fourthKey]] =
+    key;
+  for (const lock of locks) {
+    const [
+      maxHeightLock,
+      [zeroLock, firstLock, secondLock, thirdLock, fourthLock],
+    ] = lock;
+    if (maxHeightKey !== maxHeightLock) {
+      continue;
+    }
+
+    if (
+      zeroKey + zeroLock > maxHeightLock ||
+      firstKey + firstLock > maxHeightLock ||
+      secondKey + secondLock > maxHeightLock ||
+      thirdKey + thirdLock > maxHeightLock ||
+      fourthKey + fourthLock > maxHeightLock
+    ) {
+      continue;
+    }
+
+    total += 1;
+  }
+}
+
+console.log(`Part 1: ${total}`);
